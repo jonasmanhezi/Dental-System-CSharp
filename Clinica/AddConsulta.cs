6a4348b50ea454c857fa56f435f5f7b01942c992
Jonas Manhezi <67005476+jonasmanhezi@users.noreply.github.com>
2022-01-03 23:17:44 -0300

    Add files via upload

---
+++ Clinica/AddConsulta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClinicaDentaria2
+{
+    class AddConsulta
+    {
+        public void ConsultaAdd(string query)
+        {
+            //criando metodo para adicionar consultas.
+            ConnectionString Conexao = new ConnectionString();
+            SqlConnection Con = Conexao.GetCon();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Con;
+            Con.Open();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            Con.Close();
+            
+        }
+    }
+}
